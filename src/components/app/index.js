84d48f598e878ddd0f455d55be7e3da138188d2f
Eduardo Magaldi <magaldi1989@gmail.com>
2021-03-05 22:52:44 +0100

    adding initial loader

---
+++ src/components/app/index.js
@@ -0,0 +1,14 @@
+import './style.scss';
+// import Characters from '../characters';
+
+function App(props) {
+	return <>
+		<div className="star"></div>
+		<div className="app">
+			{/* <Characters /> */}
+		</div>
+	</>;
+}
+
+export default App;
+
