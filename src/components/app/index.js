806eadbb1e7e79d11b47844016d210e7168864f4
Eduardo Magaldi <magaldi1989@gmail.com>
2021-03-06 14:01:25 +0100

    doing some creative work, starting animated intro

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,11 +1,26 @@
 import './style.scss';
-// import Characters from '../characters';
+// import Loader from '../loader';
+
 
 function App(props) {
 	return <>
-		<div className="star"></div>
 		<div className="app">
-			{/* <Characters /> */}
+			<div className="content__wrapper__icon_mouse">
+				<div className="content__icon_mouse"></div>
+			</div>
+
+
+			<div className="wrapper__content">
+				<div className="content">
+
+
+
+
+
+
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+				</div>
+			</div>
 		</div>
 	</>;
 }
