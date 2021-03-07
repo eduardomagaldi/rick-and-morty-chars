6f2420569af6808aad9eb9be136320beee6fde18
Eduardo Magaldi <magaldi1989@gmail.com>
2021-03-07 22:48:23 +0100

    cleaning up

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,58 +1,28 @@
 import './style.scss';
 
-import logo from "./kisspng-portal-morty-smith-rick-sanchez-t-shirt-hoodie-portal-5ac4b0cbe2bbd5.4984654015228397559287.png";
+import rickAndMortyWithPortal from "./kisspng-portal-morty-smith-rick-sanchez-t-shirt-hoodie-portal-5ac4b0cbe2bbd5.4984654015228397559287.png";
 import title from "./../../rick-and-morty.svg";
 
-import React, { useState, useEffect } from 'react';
-
 function App(props) {
-	const [activeObj, setActiveObj] = useState({});
-
-	useEffect(() => {
-		console.log('use effect 1');
-		window.addEventListener(
-			'scroll',
-			throttle(debounce(handleScroll, 300), 300),
-		);
-	}, []);
-
-	useEffect(() => {
-		console.log('use effect 2');
-	}, [activeObj]);
-
-	function handleScroll($event) {
-		const scrollMap = {
-			'100-299': 'negative__word--rick',
-			'300-599': 'negative__word--morty',
-			'600-899': 'negative__word--complete',
-		};
-
-		const scrollY = window.scrollY;
-
-		for (const range in scrollMap) {
-			const splitRange = range.split('-');
-			const min = parseInt(splitRange[0], 10);
-			const max = parseInt(splitRange[1], 10);
-
-			if (scrollY >= min && scrollY <= max) {
-				console.log('set class');
-
-				const newActiveObj = {};
-				newActiveObj[scrollMap[range]] = 'active';
-
-				setActiveObj(newActiveObj);
-				break;
-			} else {
-				setActiveObj({});
-			}
-		}
-	}
-
 	return <>
 		<div className="app">
-			{/* <div style={{ position: 'fixed', top: 0 }}>{JSON.stringify(activeObj)}</div> */}
-
 			<section className="section__title">
+				<span
+					className="imdbRatingPlugin"
+					data-user="ur130126224"
+					data-title="tt2861424"
+					data-style="p2"
+				>
+					<a
+						href="https://www.imdb.com/title/tt2861424/?ref_=plg_rt_1"
+					>
+						<img
+							src="https://ia.media-imdb.com/images/G/01/imdb/plugins/rating/images/imdb_38x18.png"
+							alt="Rick e Morty (2013) on IMDb"
+						/>
+					</a>
+				</span>
+
 				<div className="section__title__wrapper_img_title">
 					<img
 						src={title}
@@ -81,14 +51,8 @@ function App(props) {
 			</section>
 
 			<div className="wrapper_img">
-
-				<img src={logo} className="img_main" alt="Rick and Morty with Portal" />
+				<img src={rickAndMortyWithPortal} className="img_main" alt="Rick and Morty with Portal" />
 			</div>
-
-			<div className={'negative__word negative__word--complete active ' + activeObj['negative__word--complete']}></div>
-			{/* <div className={'negative__word negative__word--rick ' + activeObj['negative__word--rick']}></div>
-			<div className={'negative__word negative__word--and ' + activeObj['negative__word--and']}></div>
-			<div className={'negative__word negative__word--morty ' + activeObj['negative__word--morty']}></div> */}
 
 			<div className="content__wrapper__icon_mouse">
 				<div className="content__icon_mouse"></div>
@@ -99,7 +63,7 @@ function App(props) {
 				<div className="content">
 					<div className="alert">
 						<h2>No actual content here?</h2>
-						<h3>Maybe you can check <a rel="nofollow" target="_blank" href="https://60453ff2ac1bd37523b45c0f--focused-brattain-bc8c35.netlify.app/1">Game of Thrones Characters</a> done by me some months ago, in Vue.js.</h3>
+						<h3>Maybe you can check <a rel="nofollow" target="_blank" href="https://60453ff2ac1bd37523b45c0f--focused-brattain-bc8c35.netlify.app/1">Game of Thrones Characters</a> done by me some months ago, in Vue.js. :-) And here is the <a href="https://github.com/eduardomagaldi/got-guys">repo</a></h3>
 					</div>
 
 					<div className="card_char">
@@ -158,41 +122,3 @@ function App(props) {
 }
 
 export default App;
-
-function debounce(func, wait = 100, immediate) {
-	let timeout;
-
-	return (...args) => {
-		const later = () => {
-			timeout = null;
-
-			if (!immediate) {
-				func(...args);
-			}
-		};
-
-		const callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-
-		if (callNow) {
-			func(...args);
-		}
-	};
-}
-
-function throttle(fn, wait) {
-	let isCalled = false;
-
-	return function (...args) {
-		if (!isCalled) {
-			fn(...args);
-
-			isCalled = true;
-
-			setTimeout(function () {
-				isCalled = false;
-			}, wait);
-		}
-	};
-}
