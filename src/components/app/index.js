cef5372cd5178f6abb56a0808a039d0317afa15e
Eduardo Magaldi <magaldi1989@gmail.com>
2021-03-07 12:34:07 +0100

    dealing with scroll position

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,15 +3,60 @@ import './style.scss';
 import logo from "./kisspng-portal-morty-smith-rick-sanchez-t-shirt-hoodie-portal-5ac4b0cbe2bbd5.4984654015228397559287.png";
 import neg from "./../../rick-and-morty-negative.svg";
 
+import React, { useState, useEffect } from 'react';
+
 function App(props) {
+	const [activeObj, setActiveObj] = useState({});
+
+	useEffect(() => {
+		console.log('use effect 1');
+		window.addEventListener('scroll', debounce(handleScroll, 500));
+		window.addEventListener('scroll', throttle(handleScroll, 500));
+		// window.addEventListener('scroll', handleScroll);
+	}, []);
+
+	useEffect(() => {
+		console.log('use effect 2');
+		// window.addEventListener('scroll', handleScroll);
+	}, [activeObj]);
+
+	function handleScroll($event) {
+		const scrollMap = {
+			'100-299': 'negative__word--rick',
+			'300-599': 'negative__word--and',
+			'600-899': 'negative__word--morty',
+		};
+		const scrollY = window.scrollY;
+
+		console.log('scrollY', scrollY);
+
+		for (const range in scrollMap) {
+			const splitRange = range.split('-');
+			const min = parseInt(splitRange[0], 10);
+			const max = parseInt(splitRange[1], 10);
+
+			if (scrollY >= min && scrollY <= max) {
+				console.log('set class');
+
+				const newActiveObj = {};
+				newActiveObj[scrollMap[range]] = 'active';
+
+				setActiveObj(newActiveObj);
+				break;
+			}
+		}
+
+	}
+
 	return <>
 		<div className="app">
+
 			<img src={logo} className="img_main" alt="Rick and Morty with Portal" />
 			{/* <img src={neg} className="img_main negative" alt="Rick and Morty with Portal" /> */}
 
-			{/* <div className="negative__word negative__word--rick"></div> */}
-			{/* <div className="negative__word negative__word--and"></div> */}
-			<div className="negative__word negative__word--morty"></div>
+			<div className={'negative__word negative__word--rick ' + activeObj['negative__word--rick']}></div>
+			<div className={'negative__word negative__word--and ' + activeObj['negative__word--and']}></div>
+			<div className={'negative__word negative__word--morty ' + activeObj['negative__word--morty']}></div>
 
 			<div className="content__wrapper__icon_mouse">
 				<div className="content__icon_mouse"></div>
@@ -23,9 +68,45 @@ function App(props) {
 				</div>
 			</div>
 		</div>
-
 	</>;
 }
 
 export default App;
 
+function debounce(func, wait = 100, immediate) {
+	let timeout;
+
+	return (...args) => {
+		const later = () => {
+			timeout = null;
+
+			if (!immediate) {
+				func(...args);
+			}
+		};
+
+		const callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+
+		if (callNow) {
+			func(...args);
+		}
+	};
+}
+
+function throttle(fn, wait) {
+	let isCalled = false;
+
+	return function (...args) {
+		if (!isCalled) {
+			fn(...args);
+
+			isCalled = true;
+
+			setTimeout(function () {
+				isCalled = false;
+			}, wait);
+		}
+	};
+}
